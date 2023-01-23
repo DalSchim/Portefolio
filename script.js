48576904b7508e5134bf93509f8eaa32d1dd8d4c
DalSchim <cabrillac123@gmail.com>
2023-01-23 17:30:46 +0100

    add scroll for my projet

--- script.js
+++ script.js
@@ -30,3 +30,26 @@ document.getElementById("topBtn").addEventListener("click", function(){
     document.body.scrollTop = 0; // Pour Safari
     document.documentElement.scrollTop = 0; // Pour Chrome, Firefox, IE et Opera
 });
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
+
+
